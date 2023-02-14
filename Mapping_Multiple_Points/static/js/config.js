6b12c2151d3944a263f1645e978932bdd2c34061
RodrigoCR25 <113931683+RodrigoCR25@users.noreply.github.com>
2023-02-14 16:47:37 -0600

    Add files via upload

---
+++ Mapping_Multiple_Points/static/js/config.js
@@ -0,0 +1,2 @@
+﻿// API key
+const API_KEY = "pk.eyJ1Ijoicm9kcmlnb2NyMjUiLCJhIjoiY2xkeG9ubWdiMGpuazNvcHJqczNsbXRvbyJ9.FNuFbBNrkd4zbh9zjeOURQ";
---
+++ Simple_Map/static/js/config.js
@@ -0,0 +1,2 @@
+﻿// API key
+const API_KEY = "ppk.eyJ1Ijoicm9kcmlnb2NyMjUiLCJhIjoiY2xkeG9ubWdiMGpuazNvcHJqczNsbXRvbyJ9.FNuFbBNrkd4zbh9zjeOURQ";
